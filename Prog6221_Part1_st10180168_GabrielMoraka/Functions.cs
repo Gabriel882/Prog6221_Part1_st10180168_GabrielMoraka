cc4fc0fb028fcd6c1012c622688d398cc5f1f905
kutlw <kutlw@DRACOMAD>
2023-04-24 23:49:14 +0200

    Add project files.

---
+++ Prog6221_Part1_st10180168_GabrielMoraka/Functions.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog6221_Part1_st10180168_GabrielMoraka
+{
+     class Functions
+    {
+
+    public    class MethodsAndFunctions
+        {
+            public string[] ingredients;
+            public int[] quantities;
+            public string[] units;
+            public string[] steps;
+
+            public MethodsAndFunctions()
+            {
+                // Initialize empty arrays for ingredients, quantities, units, and steps
+                ingredients = new string[0];
+                quantities = new int[0];
+                units = new string[0];
+                steps = new string[0];
+            }
+
+            public void ingredientDetails()
+            {
+                // Prompt the user to enter the number of ingredients
+                Console.Write("Enter the number of ingredients: ");
+                int numberOfIngredients = int.Parse(Console.ReadLine());
+
+                // Initialize the arrays with the correct size
+                ingredients = new string[numberOfIngredients];
+                quantities = new int[numberOfIngredients];
+                units = new string[numberOfIngredients];
+
+                // Prompt the user to enter the details for each ingredient
+                for (int i = 0; i < numberOfIngredients; i++)
+                {
+                    Console.WriteLine($"Enter details for ingredient #{i + 1}:");
+                    Console.Write("Name: ");
+
+                    ingredients[i] = Console.ReadLine();
+                    Console.Write("Quantity: ");
+
+                    quantities[i] = int.Parse(Console.ReadLine());
+                    Console.Write("Unit of measurement: ");
+                    units[i] = Console.ReadLine();
+
+
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write("The ingredient you entered has been successfully captured\n\n");
+                    Console.ResetColor();
+                }
+
+                // Prompt the user to enter the number of steps
+                Console.Write("Enter the number of steps: ");
+                int numberOfSteps = int.Parse(Console.ReadLine());
+
+                // Initialize the steps array with the correct size
+                steps = new string[numberOfSteps];
+
+                // Prompt the user to enter the details for each step
+                for (int i = 0; i < numberOfSteps; i++)
+                {
+                    Console.Write($"Enter step #{i + 1}: ");
+                    steps[i] = Console.ReadLine();
+
+
+                  
+                }
+
+                Console.Write("The recipe has been successfully captured\n");
+            }
+
+            public void fullDisplay()
+            {
+                // Display the ingredients and quantities
+                Console.WriteLine("Ingredients:");
+                for (int i = 0; i < ingredients.Length; i++)
+                {
+                    Console.WriteLine($" {quantities[i]} {units[i]} of {ingredients[i]}");
+                }
+
+                // Display the steps
+                Console.WriteLine("Steps:");
+                for (int i = 0; i < steps.Length; i++)
+                {
+                    Console.WriteLine($"- {steps[i]}");
+                }
+            }
+
+            public void ScaleQuantity(int factor)
+            {
+                // Multiply all the quantities by the scaling factor
+                for (int i = 0; i < quantities.Length; i++)
+                {
+                    quantities[i] *= factor;
+                }
+            }
+
+            public void ResetQuantities()
+            {
+                // Reset all the quantities to their original values
+                for (int i = 0; i < quantities.Length; i++)
+                {
+                    Console.Write("You have selected the reset quantity option\n" +
+                        "Would you like to rest the quantities\n" +
+                        "Press 1 for YES\n" +
+                        "press 2 for NO\n");
+                    int yesOrNo = Convert.ToInt32(Console.ReadLine());
+
+
+                    if (yesOrNo ==  1) 
+                    {
+                        quantities[i] /= 2;
+                        Console.Write("The quantites have been changed back to thier original values\n");
+
+                    }
+                   
+                }
+
+                 
+
+
+
+            }
+
+            public void ClearTheRecipe()
+            {
+                Console.Write("Press 1 to clear the recipe\n" +
+                    "Press 2 to cancel\n");
+                int clearData = Convert.ToInt32(Console.ReadLine());
+
+                if (clearData == 1)
+                {
+
+                    // Reset all the arrays to empty
+                    ingredients = new string[0];
+                quantities = new int[0];
+                units = new string[0];
+                steps = new string[0];
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write("The recipe has been cleared\n");
+                Console.ResetColor(); 
+                }
+
+
+                else if (clearData == 2) 
+                {
+                    Console.ForegroundColor= ConsoleColor.DarkRed;
+                    Console.Write("The Recipe is still saved");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Prog6221_Part1_st10180168_GabrielMoraka/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using static Prog6221_Part1_st10180168_GabrielMoraka.Functions;
+
+namespace RecipeApplication
+{
+  
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodsAndFunctions recipe = new MethodsAndFunctions();
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("********************************* The menu *********************************\n" +
+                    "Press 1 to enter recipe details");
+                Console.WriteLine("Press 2 to display the recipe");
+                Console.WriteLine("Press 3 to scale recipe");
+                Console.WriteLine("Press 4 to reset quantities");
+                Console.WriteLine("Press 5 to clear recipe");
+                Console.WriteLine("Press 6 to exit\n" +
+                    "********************************************************************");
+                Console.ResetColor();
+
+                string selection = Console.ReadLine();
+                switch (selection)
+                { 
+
+                    case "1":
+                    recipe.ingredientDetails();
+                    break;
+                case "2":
+                    recipe.fullDisplay();
+                    break;
+                case "3":
+                    Console.Write("Enter scaling factor as a number (half 1, double 2, or triple 3): ");
+                    double factor = int.Parse(Console.ReadLine());
+                    recipe.ScaleQuantity((int)factor);
+                    break;
+                case "4":
+                    recipe.ResetQuantities();
+                    break;
+                case "5":
+                    recipe.ClearTheRecipe();
+                    break;
+                        
+                case "6":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("Exiting program...");
+                        Console.ResetColor();
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a valid choice.");
+                    break;
+                }
+            }
+        }
+    }
+}
