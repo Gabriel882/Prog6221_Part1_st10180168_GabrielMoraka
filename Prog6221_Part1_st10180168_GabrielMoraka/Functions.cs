91e1142380126b9c0ff5e2d37cdb8cd8cf03f2c2
Gabriel Moraka <ST10180168@rcconnect.edu.za>
2023-04-26 16:53:40 +0200

    Changing the data type from double to int of the quantites variable

--- Prog6221_Part1_st10180168_GabrielMoraka/Functions.cs
+++ Prog6221_Part1_st10180168_GabrielMoraka/Functions.cs
@@ -12,7 +12,7 @@ namespace Prog6221_Part1_st10180168_GabrielMoraka
     public    class MethodsAndFunctions
         {
             public string[] ingredients;
-            public int[] quantities;
+            public double[] quantities;
             public string[] units;
             public string[] steps;
 
